f462a0baaae6690ecc6c6cd2b2850474032a5202
Dowwnfall <artamation@gmail.com>
2022-03-16 19:15:11 +0000

    finished form-inputs, also added a footer that helps you edit your links and sections.  I wanted to do this in seprate commits but im new to git so I cant figure out how to seprate this into 2 commits

--- src/Components/ListRow/ListLinks.js
+++ src/Components/ListRow/ListLinks.js
@@ -1,8 +1,16 @@
+import { useContext } from "react";
+import ListDataContext from "../../store/list-data";
+
 const ListLinks = (props) => {
+    const {delListItem} = useContext(ListDataContext);
+    const {isHidden} = useContext(ListDataContext);
     return (
         <>
-            {props.links.map(link => (
-                <a href={link.linkUrl}>{link.linkTitle}</a>
+            {props.links.map((link, index) => (
+                <div key={index}>
+                    <a href={link.linkUrl}>{link.linkTitle}</a>
+                    {isHidden && <button onClick={() => delListItem(props.categoryIndex, index)}>x</button>}
+                </div>
             ))}
         </>
     )
--- src/Components/ListRow/ListRow.js
+++ src/Components/ListRow/ListRow.js
@@ -1,21 +1,25 @@
 import React, {useContext} from 'react';
 import styles from './ListRow.module.css'
+
 import ListLinks from './ListLinks';
 import ListDataContext from '../../store/list-data';
 const ListRow = (props) => {
+    const {addToList} = useContext(ListDataContext);
     const {list} = useContext(ListDataContext);
+    const {isHidden} = useContext(ListDataContext);
       return (
         <>
-            {list.map(List => (
-                <div className={styles.container}>
-                    <h2 className={styles.line}>{List.title}</h2>
-                    <ListLinks links={List.links}/>
+            {list.map((list, index) => (
+                <div className={styles.container} key={index + 1}>
+                    <h2 className={styles.line}>{list.title}</h2>
+                    <ListLinks categoryIndex={index} links={list.links}/>
+                    {isHidden && <a onClick={() => props.onConfirmEdit(index + 1)}>+link</a>}
                 </div>
             ))}
-            <div onClick={props.onConfirmNew} className={`${styles.add} ${styles.container}`}>
+            {/* {isHidden && <div onClick={props.onConfirmNew} className={`${styles.add} ${styles.container}`}>
                 <h2>Add Another List</h2>
                 <div className={styles.plus}>+</div>
-            </div>
+            </div>} */}
             
         
         </>
